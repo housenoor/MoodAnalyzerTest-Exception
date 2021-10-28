54c1fcc8255cb607309f0172a8282ae5f43f9278
Srikanth <srikanthh994@gmail.com>
2021-10-28 13:47:06 +0530

    Added:Mood Analyze test

--- MoodAnalyseTest.java
+++ MoodAnalyseTest.java
@@ -4,21 +4,12 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class MoodAnalyseTest {
-	// testCase to predict the mood Sad
 	@Test
-	public void givenMessage_SadMood_Should_Return_Sad() {
-		MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("I am In a Sad Mood");
-		String mood = moodAnalyzer.analyseMood();
-		// Asserting The Result As Boolean Type True Or False
+	public void given_SadMood_Should_Return_Sad() {
+		MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain();
+		String mood = moodAnalyzer.analyseMood("I am In a Sad Mood");
 		Assertions.assertEquals("SAD", mood);
 
 	}
 
-	// Constructor in message should return Happy
-	@Test
-	public void givenMessage_AnyMood_Should_Return_HAPPY() {
-		MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am in Happy Mood");
-		String mood = moodAnalyser.analyseMood();
-		Assertions.assertEquals("HAPPY", mood);
-	}
 }
--- MoodAnalyzerMain.java
+++ MoodAnalyzerMain.java
@@ -1,29 +1,14 @@
 package com.bl.moodanalyzerexception;
 
 public class MoodAnalyzerMain {
-	private String message;
-
 	// Constructor
 	public MoodAnalyzerMain() {
 	}
 
-	// paraMetarised constructor
-	public MoodAnalyzerMain(String message) {
-		this.message = message;
-	}
-
-	public String analyseMood() throws MoodAnalysisException {
-		try {
-
-			if (this.message.contains("Sad"))
-				return "SAD";
-			else
-				return "HAPPY";
-		} catch (Exception e) {
-			if (message == null)
-				throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.NULL);
-			else
-				throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.EMPTY);
-		}
+	public String analyseMood(String message) {
+		if (message.contains("Sad"))
+			return "SAD";
+		else
+			return "HAPPY";
 	}
 }
